9e783dd0b7ccf8e82f05740c73e79d3e34334c3b
mdroof <mdroof@svsu.edu>
2015-07-22 11:28:59 -0400

    Fixes webclient

--- app/src/main/java/com/example/mdroof/cbooks/MainActivity.java
+++ app/src/main/java/com/example/mdroof/cbooks/MainActivity.java
@@ -164,23 +164,12 @@ public class MainActivity extends ActionBarActivity
 
 
             webview.setWebViewClient(new WebViewClient() {
-                /*
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    view.loadUrl(url);
-                    return true;
-                }
-
-                @Override
                 public void onPageFinished(WebView view, String url) {
-                    super.onPageFinished(view, url);
-                    if (mProgress.isShowing()) {
-                        mProgress.dismiss();
-                    }
-                    Log.v("MyActivity - AfterLoad", view.getUrl());
-                }*/
+                    // do your stuff here
+                }
             });
-            webview.loadUrl(getString(R.string.home_url));
+
+            //webview.loadUrl(getString(R.string.home_url));
             Log.v("MyActivity", webview.getUrl());
             return rootView;
         }
